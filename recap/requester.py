f6813713426d4ef0af490d0b9253a8a9709dbe86
Croydon <cr0ydon@outlook.com>
2019-01-14 10:44:24 +0100

    Implement requester.get_daily_users()

--- recap/requester.py
+++ recap/requester.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
+import requests
+import json
+
 
 class requester():
     def __init__(self):
@@ -10,9 +14,31 @@ class requester():
         pass
 
     @staticmethod
-    def _transform_data(json):
-        return None
+    def _validate_date_format(date_text):
+        try:
+            datetime.strptime(date_text, '%Y%m%d')
+        except ValueError:
+            raise requester.ExceptionInvalidDates("Incorrect date format. Use YYYYMMDD format.")
+
+    @staticmethod
+    def _transform_data(json_data):
+        return json.loads(json_data)
 
     @staticmethod
     def get_daily_users(start_date="20181101", end_date="20181130"):
-        return None
+        """ Basic checks first if dates are valid
+        """
+        if start_date > end_date:
+            raise requester.ExceptionInvalidDates("Start date is after end date!")
+
+        requester._validate_date_format(start_date)
+        requester._validate_date_format(end_date)
+
+        api = "https://addons.mozilla.org/en-US/firefox/addon/vertical-tabs-reloaded/statistics/usage-day-{}-{}.json".format(
+            start_date,
+            end_date
+        )
+
+        r = requests.get(api)
+
+        return requester._transform_data(r.text)

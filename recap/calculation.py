0238f3e0d53741c6593fd486128f565abc47695e
Croydon <cr0ydon@outlook.com>
2019-01-14 11:16:14 +0100

    Calculation: Implement average

--- recap/calculation.py
+++ recap/calculation.py
@@ -12,28 +12,31 @@ class calculation():
         rounded as there aren't half users :) 
     """
     @staticmethod
-    def average(numbers):
+    def average(numbers: list):
+        user_sum = 0
+        for day in numbers:
+            user_sum += day
+        return round(user_sum / len(numbers))
+
+    @staticmethod
+    def min(numbers: list):
         return None
 
     @staticmethod
-    def min(numbers):
+    def max(numbers: list):
         return None
 
     @staticmethod
-    def max(numbers):
+    def biggest_increase(numbers: list):
         return None
 
     @staticmethod
-    def biggest_increase(numbers):
-        return None
-
-    @staticmethod
-    def biggest_decrease(numbers):
+    def biggest_decrease(numbers: list):
         return None
 
     """ Total change of users, over the a time span,
         basically first - last number
     """
     @staticmethod
-    def total_change(numbers):
+    def total_change(numbers: list):
         return None
